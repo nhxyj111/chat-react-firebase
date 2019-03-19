1473ab23d9f1a3e75fb0cf3b4ae45b9a068793ba
xuyejian <nhxyj111@gmail.com>
2019-03-19 12:37:07 +0800

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import firebase from "firebase";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBaMEM_zpBs_elceLej5bXcr-dW3wU16HA",
+  authDomain: "chat-app-ad235.firebaseapp.com",
+  databaseURL: "https://chat-app-ad235.firebaseio.com",
+  projectId: "chat-app-ad235",
+  storageBucket: "chat-app-ad235.appspot.com",
+  messagingSenderId: "583053007167"
+};
+firebase.initializeApp(config);
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Nav">
+        <div className="User">
+          <img
+            className="UserImage"
+            alt="whatever"
+            src="https://placekitten.com/64/64"
+          />
+          <div>
+            <div>Xu Yj</div>
+            <div>
+              <button className="text-button">log out</button>
+            </div>
+          </div>
+        </div>
+        <nav className="ChannelNav">
+          <a href="/channel/awesome"># awesome</a>
+          <a className="active" href="/channel/general">
+            # general
           </a>
-        </header>
+        </nav>
       </div>
-    );
-  }
+      <div className="Channel">
+        <div className="ChannelMain">
+          <div className="ChannelInfo">
+            <div className="Topic">
+              Topic: <input className="TopicInput" value="Awesome stuff" />
+            </div>
+            <div className="ChannelName">#general</div>
+          </div>
+          <div className="Messages">
+            <div className="EndOfMessages">That's every message!</div>
+            <div>
+              <div className="Day">
+                <div className="DayLine" />
+                <div className="DayText">12/6/2018</div>
+                <div className="DayLine" />
+              </div>
+              <div className="Message with-avatar">
+                <div className="Avatar" />
+                <div className="Author">
+                  <div>
+                    <span className="UserName">Ryan Florence </span>
+                    <span className="TimeStamp">3:37 PM</span>
+                  </div>
+                  <div className="MessageContent">Alright, lets do this.</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="Message no-avatar">
+                <div className="MessageContent">works now?</div>
+              </div>
+            </div>
+          </div>
+          <div className="ChatInputBox">
+            <input className="ChatInput" placeholder="Message #general" />
+          </div>
+        </div>
+        <div className="Members">
+          <div>
+            <div className="Member">
+              <div className="MemberStatus offline" />
+              Ryan Florence
+            </div>
+            <div className="Member">
+              <div className="MemberStatus online" />
+              cleverbot
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
